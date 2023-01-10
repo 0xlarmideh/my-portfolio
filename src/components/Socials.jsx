922488adf1807d8b02508d599b7d770f71b0819c
0xlarmideh <busarioyewole@gmail.com>
2023-01-10 18:29:34 +0100

    FEAT/Added data for the projects section

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
 
+const datas = [
+  { id: 1, title: "Twitter", icon: "", url: "" },
+  { id: 2, title: "LinkedIn", icon: "", url: "" },
+  { id: 3, title: "Resume", icon: "", url: "" },
+  { id: 4, title: "GitHub", icon: "", url: "" },
+];
 export default function Socials() {
+  const socialsLink = datas.map(({id, title, icon, url}) => {
+    return (
+      <div className="flex gap-[10px] flex-row " key={id}>
+        <a href={url}></a>
+        <div>{title} </div>
+        <img src={icon}></img>
+      </div>
+    );
+  })
   return (
-    <div>Socials</div>
+    <div className='flex gap-[40px] '>{socialsLink} </div>
   )
 }
