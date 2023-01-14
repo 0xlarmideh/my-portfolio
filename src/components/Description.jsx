33663ed3f951ffd86827c858a0403793107fc04d
0xlarmideh <busarioyewole@gmail.com>
2023-01-14 17:39:32 +0100

    STYLE/Changed font stack

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -6,19 +6,19 @@ const Description = () => {
     <div>
       <Heading
         title="Abdulhameed Busari"
-        className="text-white max-laptop:text-[46px] mb-[15px] text-[66px] leading-[56px] head-text"
+        className="text-white max-laptop:text-[46px] mb-[15px] text-[64px] leading-[56px] head-text"
       />
-      <SmallHeading
-        title="Front-End Developer"
+      <Paragraph
+        title="Frontend Developer"
         className="text-white mb-[20px] text-[27px] mb-[45px] w-[70%] font-bold tracking-wide "
       />
       <Paragraph
         title="An architect and programmer in love with tech. Delivering top-notch user-centric designs with great meticulousness. "
-        className="text=[16px] leading-[27px] max-md:leading-[22px]  w-[80%] max-md:w-[100%] max-md:text-[14px] "
+        className="text-[21px] leading-[27px] max-md:leading-[22px]  w-[90%] max-md:w-[100%] max-md:text-[14px] mb-[17px] "
       />
       <Paragraph
         title="I have developed some beautiful projects. I am open to FrontEnd Developer Intern and Junior roles. "
-        className="text=[16px] leading-[27px] max-md:leading-[22px] w-[80%] max-md:w-[100%] max-md:mb-[20px] max-md:text-[14px] "
+        className="text-[21px] leading-[27px] max-md:leading-[22px] w-[90%] max-md:w-[100%] max-md:mb-[20px] max-md:text-[14px] "
       />
     </div>
   );
