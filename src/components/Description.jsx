c8e2ce4d71681fbf57898d1faa3b124068bd48b9
0xlarmideh <busarioyewole@gmail.com>
2023-01-21 01:14:00 +0100

    FEAT/Improved description

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -9,16 +9,17 @@ const Description = () => {
         className="text-white max-laptop:text-[53px]  text-[66px] max-laptop:leading-[45px] leading-[56px]"
       />
       <Paragraph
+        data-aos="fade-in"
         title="Frontend Developer"
-        className="text-white mb-[20px] text-[15px] mb-[45px]  uppercase"
+        className="text-white mb-[20px] text-[15px] mb-[35px]  uppercase"
       />
       <Paragraph
-        title="An architect and programmer in love with tech. Delivering top-notch user-centric designs with great meticulousness. "
-        className="text-[21px] leading-[27px] max-md:leading-[22px]  w-[90%] max-md:w-[100%] max-md:text-[16px] mb-[10px] "
+        title="I am an experienced professional with a background in architecture and a passion for frontend development. I pride myself on my strong attention to detail and ability to create visually appealing and intuitive designs. "
+        className="text-[18px] leading-[27px] max-md:leading-[22px]  w-[100%] max-md:w-[100%] mb-[10px] "
       />
       <Paragraph
-        title="I have developed some beautiful projects. I am open to Frontend Developer Intern and Junior roles. "
-        className="text-[21px] leading-[27px] max-md:leading-[22px] w-[90%] max-md:w-[100%] max-md:mb-[20px] max-md:text-[16px] "
+        title="I am skilled in using various technologies for frontend development, always looking to improve and stay current with industry trends. I am excited to continue to push the boundaries of what is possible and create visually stunning and functional designs."
+        className="text-[18px] leading-[27px] max-md:leading-[22px] w-[100%] max-md:w-[100%] max-md:mb-[20px] "
       />
     </div>
   );
