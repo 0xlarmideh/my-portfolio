9b9ec92d4256efad7f4704333408d3641afd7d94
Abdulhameed <busarioyewole@gmail.com>
2024-05-30 06:29:04 +0100

    modify style

--- src/App.jsx
+++ src/App.jsx
@@ -12,23 +12,28 @@ import Loading from "./components/LoadingLogo";
 
 function App() {
   const [loading, setLoading] = useState(false);
-  
+
   useEffect(() => {
-    AOS.init({ duration: 1000, startEvent: "DOMContentLoaded", offset: 0, once: true });
-    setLoading(true)
+    AOS.init({
+      duration: 1000,
+      startEvent: "DOMContentLoaded",
+      offset: 0,
+      once: true,
+    });
+    setLoading(true);
     setTimeout(() => {
-      setLoading(false)
-    }, 3000);
+      setLoading(false);
+    }, 1500);
   }, []);
 
   return loading ? (
     <Loading />
   ) : (
-    <div className="max-md:block ">
+    <div className="max-md:block">
       <div className="grid-1 max-md:relative fixed w-[50%] max-md:w-[100%] ">
         <Overview />
       </div>
-      <div className="grid-2 p-[36px] pb-[0] mt-[60px] w-[50%] max-md:w-[100%] absolute right-0 mr-[60px] max-laptop:m-[0px] ">
+      <div className="grid-2 overflow-hidden p-[36px] pb-[0] mt-[60px] w-[50%] max-md:w-[100%] absolute right-0 mr-[60px] max-laptop:m-[0px] ">
         <Projects />
         <Articles />
         <Stack />
